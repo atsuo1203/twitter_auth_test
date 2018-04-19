8305fa3639daa871532f73b41dcdb92abff49ed4
atsuo1203 <dondonkitemasu@gmail.com>
2018-04-20 01:21:45 +0900

    不要コメント削除

--- src/login.js
+++ src/login.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 
 class Login extends Component {
-  componentWillMount = () => {
-    console.log('componentWillMount')
-  }
-
-  componentDidMount = () => {
-    console.log('componentDidMount')
-  }
-
-  async get_data() {
-    const response = await axios({
-      method: 'get',
-      url: 'http://127.0.0.1:5000/',
-    });
-    console.log(response)
-  }
-
-  handleCache = () => {
-    // localStorage.setItem('hogehoge', 'hugahuga')
-  }
-
   handleAuthorize = () => {
     document.location.href = 'http://127.0.0.1:5000/authorize'
   }
--- src/profile.js
+++ src/profile.js
@@ -1,14 +1,6 @@
 import React, { Component } from 'react';
 
 class Profile extends Component {
-  componentWillMount = () => {
-    console.log('componentWillMount')
-  }
-
-  componentDidMount = () => {
-    console.log('componentDidMount')
-  }
-
   render() {
     return (
       <div>
